866461916c463adf0358c9015791cd3a0f507d49
Ben Partridge <bpartridge@atlassian.com>
2020-03-05 15:11:07 +1100

    add documentation to MigrationServiceV2 class

--- src/main/java/com/atlassian/migration/datacenter/spi/MigrationServiceV2.java
+++ src/main/java/com/atlassian/migration/datacenter/spi/MigrationServiceV2.java
@@ -2,14 +2,31 @@ package com.atlassian.migration.datacenter.spi;
 
 import com.atlassian.migration.datacenter.dto.Migration;
 
+/**
+ * Manages the lifecycle of the migration
+ */
 public interface MigrationServiceV2 {
 
+    /**
+     * Creates a new migration in the initial stage
+     */
     Migration createMigration();
 
+    /**
+     * Gets the current stage of the migration
+     */
     MigrationStage getCurrentStage();
 
+    /**
+     * Progresses to the next stage of the migration.
+     * @see MigrationStage#getNext()
+     */
     void nextStage();
 
+    /**
+     * Moves the migration into an error stage
+     * @see MigrationStage#ERROR
+     */
     void error();
 
 }
