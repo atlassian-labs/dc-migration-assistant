d515cc4b94430f91767d01b8f5beeafd09c8ce54
Varun Arbatti <1063972+theghostwhoforks@users.noreply.github.com>
2020-05-05 11:31:04 +1000

    CHET-23: Conditionally render either a restart migration or link to a current migration path from error page

--- frontend/src/main/dc-migration-assistant-fe/components/MigrationError.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/MigrationError.tsx
@@ -14,27 +14,83 @@
  * limitations under the License.
  */
 
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, ReactElement, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { I18n } from '@atlassian/wrm-react-i18n';
 
 import { migration, MigrationStage } from '../api/migration';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Button from '@atlaskit/button';
 import { homePath } from '../utils/RoutePaths';
+import { getPathForStage } from '../utils/migration-stage-to-path';
 
 const MigrationErrorContainer = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
 `;
+
+type ResetMigrationProps = {
+    currentStage: MigrationStage;
+    resetMigrationFunc: VoidFunction;
+    errorContent: string;
+};
+
+const StageAwareMigrationSection = ({
+    resetMigrationFunc,
+    currentStage,
+    errorContent,
+}: ResetMigrationProps): ReactElement => {
+    const learnMoreText = I18n.getText('atlassian.migration.datacenter.common.learn_more');
+
+    if (currentStage === MigrationStage.ERROR) {
+        return (
+            <>
+                <h2>Error</h2>
+                <p>
+                    The Migration assistant entered an error state <br />
+                    Click here to{' '}
+                    <a
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        href="https://confluence.atlassian.com/jirakb/how-to-use-the-data-center-migration-app-to-migrate-jira-to-an-aws-cluster-1005781495.html?#HowtousetheDataCenterMigrationapptomigrateJiratoanAWScluster-errors"
+                    >
+                        {learnMoreText.toLowerCase()}
+                    </a>{' '}
+                    about why this may have occurred
+                    {errorContent} <br />
+                </p>
+                <Button onClick={resetMigrationFunc} appearance="primary">
+                    {I18n.getText('atlassian.migration.datacenter.error.reset.button')}
+                </Button>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <p>
+                This is an error page that is shown only when a migration fails. You currently have
+                a migration in progress.
+            </p>
+            <p>Click the button below to view the latest status.</p>
+            <Link to={getPathForStage(currentStage)}>
+                <Button appearance="primary">
+                    {I18n.getText('atlassian.migration.datacenter.error.current.stage')}
+                </Button>
+            </Link>
+        </>
+    );
+};
 
 export const MigrationError: FunctionComponent = () => {
     const [isInErrorState, setIsInErrorState] = useState<boolean>(false);
+    const [currentStage, setCurrentStage] = useState<MigrationStage>(MigrationStage.NOT_STARTED);
     const [redirectToNewMigration, setRedirectToNewMigration] = useState<boolean>(false);
 
     useEffect(() => {
         migration.getMigrationStage().then((stage: string) => {
+            setCurrentStage(stage as MigrationStage);
             if (stage === MigrationStage.ERROR.valueOf()) {
                 setIsInErrorState(true);
             }
@@ -51,20 +107,11 @@ export const MigrationError: FunctionComponent = () => {
         <>
             {redirectToNewMigration && <Redirect to={homePath} push />}
             <MigrationErrorContainer>
-                <h2>Error</h2>
-                <p>
-                    This is the skeleton error page for the migration plugin{' '}
-                    <a
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        href="https://confluence.atlassian.com/jirakb/how-to-use-the-data-center-migration-app-to-migrate-jira-to-an-aws-cluster-1005781495.html"
-                    >
-                        {I18n.getText('atlassian.migration.datacenter.common.learn_more')}
-                    </a>
-                </p>
-                <Button isDisabled={!isInErrorState} onClick={resetMigration} appearance="primary">
-                    {I18n.getText('atlassian.migration.datacenter.error.reset.button')}
-                </Button>
+                <StageAwareMigrationSection
+                    currentStage={currentStage}
+                    resetMigrationFunc={resetMigration}
+                    errorContent=""
+                />
             </MigrationErrorContainer>
         </>
     );
