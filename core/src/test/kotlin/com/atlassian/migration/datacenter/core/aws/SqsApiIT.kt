9154bb176ebd5d386af5af33fc349728ff6e6951
Varun Arbatti <1063972+theghostwhoforks@users.noreply.github.com>
2020-06-02 11:27:14 +1000

    CHET-443: Integration test for queue messages and incorrect queue url

--- core/src/test/kotlin/com/atlassian/migration/datacenter/core/aws/SqsApiIT.kt
+++ core/src/test/kotlin/com/atlassian/migration/datacenter/core/aws/SqsApiIT.kt
@@ -2,16 +2,20 @@ package com.atlassian.migration.datacenter.core.aws
 
 import cloud.localstack.docker.LocalstackDockerExtension
 import cloud.localstack.docker.annotation.LocalstackDockerProperties
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.Test
+import com.atlassian.migration.datacenter.core.exceptions.AwsQueueConnectionException
+import com.atlassian.migration.datacenter.core.exceptions.AwsQueueError
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.extension.ExtendWith
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.services.sqs.model.CreateQueueRequest
+import software.amazon.awssdk.services.sqs.model.DeleteQueueRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequestEntry
 import java.net.URI
 import java.util.function.Supplier
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @Tag("integration")
 @ExtendWith(LocalstackDockerExtension::class)
@@ -41,8 +45,34 @@ class SqsApiIT {
     }
 
     @Test
+    fun shouldRaiseExceptionWhenQueueUrlIsIncorrect() {
+        Assertions.assertThrows(AwsQueueConnectionException::class.java) { sqs.getQueueLength("$queueUrl/foo") }
+    }
+
+    @Test
     fun shouldGetZeroQueueLengthWhenNoMessagesArePresentInTheQueue() {
         val queueLength = sqs.getQueueLength(queueUrl)
         assertEquals(0, queueLength)
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        val deleteQueueCf = sqsAsyncClient.deleteQueue(DeleteQueueRequest.builder().queueUrl(queueUrl).build())
+        deleteQueueCf.get()
+    }
+
+    @Test
+    fun shouldGetCorrectQueueLengthWhenMessagesArePresentInTheQueue() {
+        val sendMessageBatchCf = sqsAsyncClient.sendMessageBatch(SendMessageBatchRequest.builder().queueUrl(queueUrl).entries(
+                SendMessageBatchRequestEntry.builder().id("1").messageBody("{\"path\":\"/foo/bar/1\"}").build(),
+                SendMessageBatchRequestEntry.builder().id("2").messageBody("{\"path\":\"/foo/bar/2\"}").build(),
+                SendMessageBatchRequestEntry.builder().id("3").messageBody("{\"path\":\"/foo/bar/3\"}").build()
+        ).build())
+        val sendMessageBatchResponse = sendMessageBatchCf.get()
+        assertTrue(sendMessageBatchResponse.hasSuccessful())
+        assertEquals(3, sendMessageBatchResponse.successful().size)
+
+        val queueLength = sqs.getQueueLength(queueUrl)
+        assertEquals(3, queueLength)
     }
 }
