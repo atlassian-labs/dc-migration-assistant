dd4d826824242ae392f2e1b38390dac83f6dae9e
Varun Arbatti <1063972+theghostwhoforks@users.noreply.github.com>
2020-06-05 14:13:32 +1000

    CHET-445: Add integration test to move to check for drain only when state is in FS sync

--- core/src/main/kotlin/com/atlassian/migration/datacenter/core/fs/captor/SqsQueueWatcher.kt
+++ core/src/main/kotlin/com/atlassian/migration/datacenter/core/fs/captor/SqsQueueWatcher.kt
@@ -20,9 +20,11 @@ import com.atlassian.migration.datacenter.core.aws.SqsApi
 import com.atlassian.migration.datacenter.spi.MigrationService
 import com.atlassian.migration.datacenter.spi.MigrationStage
 import com.atlassian.migration.datacenter.spi.MigrationStage.FINAL_SYNC_WAIT
+import org.slf4j.LoggerFactory
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import java.util.function.BiConsumer
 
 //TODO: Convert to using co-routines and suspend functions?
 class SqsQueueWatcher(private val sqsAPi: SqsApi,
@@ -31,11 +33,32 @@ class SqsQueueWatcher(private val sqsAPi: SqsApi,
 
     constructor(sqsAPi: SqsApi, migrationService: MigrationService) : this(sqsAPi, migrationService, 30)
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(SqsQueueWatcher::class.java)
+    }
+
     override fun awaitQueueDrain(): Boolean {
-        awaitRunnableToComplete(::checkForStateToBeInFsSyncAwait).get()
-        awaitRunnableToComplete(::checkForQueueToBeEmpty).get()
-        migrationService.transition(MigrationStage.VALIDATE)
-        return true
+        //TODO: Change from when complete to toAccept/toCombine idioms
+        val completableFuture = this.awaitRunnableToComplete(::checkForStateToBeInFsSyncAwait)
+                .whenComplete { _, _ ->
+                    run {
+                        awaitRunnableToComplete(::checkForQueueToBeEmpty).get()
+                    }
+                }
+                .whenComplete(BiConsumer { _, _ ->
+                    run {
+                        migrationService.transition(MigrationStage.VALIDATE)
+                    }
+                })
+
+        try {
+            completableFuture.get()
+            logger.info("Successfully waited for queue to be drained. Transitioned state to {}", MigrationStage.VALIDATE)
+            return true
+        } catch (e: Exception) {
+            logger.error("Error while waiting for queue to be drained", e)
+        }
+        return false
     }
 
     private fun checkForQueueToBeEmpty(future: @ParameterName(name = "future") CompletableFuture<Unit>): Runnable {
