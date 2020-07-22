309012f35e63d931c67b235e253b6064f6e8be05
Adam Brokes <abrokes@atlassian.com>
2020-07-22 12:27:31 +1000

    CHET-623: Use and defaults username and password

--- jira-e2e-tests/cypress/support/index.js
+++ jira-e2e-tests/cypress/support/index.js
@@ -39,10 +39,10 @@ export const amps_context = gen_context('http://localhost:2990', '/jira');
 export const devserver_context = gen_context('http://localhost:3333', '');
 export const compose_context = gen_context('http://jira:8080', '/jira');
 
-Cypress.Commands.add('jira_login', (ctx, uname, passwd) => {
+Cypress.Commands.add('jira_login', (ctx, uname = 'admin', passwd = 'admin') => {
     cy.visit(ctx.loginURL);
 
-    cy.get('#login-form-username').type('admin');
+    cy.get('#login-form-username').type(uname);
     cy.get('#login-form-password').type(passwd);
     cy.get('#login-form-submit').click();
     // Force wait for dashboard to avoid flakiness.
@@ -50,7 +50,7 @@ Cypress.Commands.add('jira_login', (ctx, uname, passwd) => {
 
     // Ensure we have full admin access before doing anything
     cy.visit(ctx.sudoURL);
-    cy.get('#login-form-authenticatePassword').type(passwd);
+    cy.get('#login-form-authenticatePassword').type(uname);
     cy.get('#login-form-submit').click();
 });
 
