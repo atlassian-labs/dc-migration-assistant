0d1ef70494ba3d0e677c8677e9ee572f09d6e0bb
Adam Brokes <abrokes@atlassian.com>
2020-07-29 17:37:18 +1000

    CHET-627: Use validation step

--- jira-e2e-tests/cypress/support/pages/FinalSync.ts
+++ jira-e2e-tests/cypress/support/pages/FinalSync.ts
@@ -19,16 +19,8 @@ export const monitorFinalSync = (ctx: AppContext) => {
 
     waitForStatus(
         `${ctx.context}/rest/dc-migration/1.0/migration`,
-        'DATA_MIGRATION_IMPORT',
+        'validation',
         EndpointType.STAGE
-    );
-
-    cy.get('#dc-migration-assistant-root h4').contains('Database import', { timeout: 20000 });
-
-    waitForStatus(
-        `${ctx.context}/rest/dc-migration/1.0/migration/final-sync/status`,
-        'DONE',
-        EndpointType.FINAL_SYNC
     );
 
     cy.get('button[data-testid=button-next').should('be.visible').click();
