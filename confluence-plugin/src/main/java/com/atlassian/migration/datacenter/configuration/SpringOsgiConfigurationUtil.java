a48e916359a98b367f2bbccab877e5e63716f61d
Ben Partridge <bpartridge@atlassian.com>
2020-11-04 13:22:57 +1100

    Clean up dependencies for confluence plugin

--- confluence-plugin/src/main/java/com/atlassian/migration/datacenter/configuration/SpringOsgiConfigurationUtil.java
+++ confluence-plugin/src/main/java/com/atlassian/migration/datacenter/configuration/SpringOsgiConfigurationUtil.java
@@ -16,8 +16,6 @@
 
 package com.atlassian.migration.datacenter.configuration;
 
-import com.atlassian.util.concurrent.LazyReference;
-
 import java.util.function.Supplier;
 
 import static com.atlassian.plugins.osgi.javaconfig.OsgiServices.importOsgiService;
@@ -30,15 +28,6 @@ public class SpringOsgiConfigurationUtil {
      * Works around the Spring Configuration problem when we try to import the class which is not available yet.
      */
     public static <T> Supplier<T> lazyImportOsgiService(Class<T> clazz) {
-        return toSupplier(new LazyReference<T>() {
-            @Override
-            protected T create() {
-                return importOsgiService(clazz);
-            }
-        });
-    }
-
-    private static <T> Supplier<T> toSupplier(LazyReference<T> lazyReference){
-        return lazyReference::get;
+        return () -> importOsgiService(clazz);
     }
 }
